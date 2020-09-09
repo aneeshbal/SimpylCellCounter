140cb619e6db05809dd681110661ab0b336b44e2
Aneesh Bal <aneesh.s.bal@gmail.com>
2020-09-09 15:21:24 -0400

    Update scc2.py

--- scc/2x/scc2.py
+++ scc/2x/scc2.py
@@ -75,7 +75,7 @@ class ProcessMasks:
 		for i,j in zip(centersx,centersy):
 		  cv2.circle(image_to_paste, (int(j), int(i)), 5, (255), 1)
 		  
-		return len(markers), image_to_paste
+		return len(centersx), image_to_paste
 		
 		
 		
