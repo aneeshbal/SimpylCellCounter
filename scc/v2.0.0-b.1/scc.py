12a10c0b5037fe2f5b6aa71dd7a20cd2f0394af8
Chris Reeves <38331558+reevesc7@users.noreply.github.com>
2021-10-08 14:29:07 -0400

    Revert "Updated scc.py"

--- scc/v2.0.0-b.1/scc.py
+++ scc/v2.0.0-b.1/scc.py
@@ -4,49 +4,41 @@ import cv2
 import numpy as np
 import pandas as pd
 import configparser
-import imghdr
 
 #Create 'config.ini' if it doesn't exist in the current directory
 if os.path.isfile('config.ini') == False:
 	cfgFile = open('config.ini','w')
 	print('No config file detected!\nGenerating config file.', flush=True)
 	lines = [
-		'[GENERAL]\n'
-		'mode = debug\n'
-		'	#(string) - (count, thresh, debug). What mode SCC will operate in.\n'
-		'		#\'count\' will count the cells in each image\n'
-		'		#\'thresh\' will quickly return background and threshold values\n'
-		'		#\'debug\' will output debug images and csv columns.\n'
-		'fluorescent = False\n'
-		'	#(bool) - Change to true to process fluorescence images\n'
-		'threshMethod = rel\n'
-		'	#(string) - (abs: absolute, rel: relative, dif: difference). See sections below.\n'
-		'imageLocation = 10X images\n'
-		'	#(string) - Directory in which the images to be processed are located. Can be full path or path relative to scc.py\n'
-		'csvOutputLocation = \n'
-		'	#(string) - May be left blank. Directory into which output csv file with cell counts will be placed. If blank, will write to imageLocation. Can be full path or path relative to scc.py\n'
-		'imageOutputLocation = \n'
-		'	#(string) - May be left blank. Directory into which debug outputs will be placed. If blank, will write to directory of the input images. Can be full path or path relative to scc.py\n\n'
-		'[CELL FEATURES]\n'
-		'size = 13\n'
-		'	#(int) - Approximate size (radius in pixels) of cells to count.\n'
-		'minAreaCoeff = 0.8\n'
-		'	#(float) - Coefficient of the formula which determines the minimum area a cell can be. (A = size^2 * minAreaCoeff)\n'
-		'maxAreaCoeff = 4.5\n'
-		'	#(float) - Coefficient of the formula which determines the maximum area a cell can be. (A = size^2 * maxAreaCoeff)\n'
-		'circularityThresh = 0.9\n'
-		'	#(float) - How circular a cell\'s contour must be before it is counted.\n\n'
-		'#Process Thresholds determine the brightness used to create the first black-and-white image.\n'
-		'#Cell Thresholds determine the brightness below which the average pixel brightness within a contour must be to be counted as a cell.\n'
-		'[ABSOLUTE THRESHOLD] - (int) Uses a single threshold value across images. Higher values are less restrictive.\n'
-		'processThresh = 144\n'
-		'cellThresh = 134\n\n'
-		'[RELATIVE THRESHOLD] - (float) Determines threshold value for each image as a proportion of background brightness. Higher values are less restrictive.\n'
-		'processThresh = 0.83\n'
-		'cellThresh = 0.78\n\n'
-		'[DIFFERENCE THRESHOLD] - (int) Determines threshold for each image as a difference from background brightness. Higher values are more restrictive.\n'
-		'processThresh = 30\n'
-		'cellThresh = 40'
+		'[GENERAL]'
+		'\nmode = debug'
+		'\n	#(string) - (count, thresh, debug). What mode SCC will operate in.'
+		'\n		#\'count\' will count the cells in each image'
+		'\n		#\'thresh\' will quickly return background and threshold values'
+		'\n		#\'debug\' will output debug images and csv columns.'
+		'\nimageLocation = 10X images'
+		'\n	#(string) - Directory in which the images to be processed are located. Can be full path or path relative to scc.py'
+		'\ncsvOutputLocation = '
+		'\n	#(string) - May be left blank. Directory into which output csv file with cell counts will be placed. If blank, will write to imageLocation. Can be full path or path relative to scc.py'
+		'\nimageOutputLocation = '
+		'\n	#(string) - May be left blank. Directory into which debug outputs will be placed. If blank, will write to directory of the input images. Can be full path or path relative to scc.py'
+		'\nsize = 13'
+		'\n	#(int) - Approximate size (radius) of cells to count.'
+		'\ncircularityThresh = 0.9'
+		'\n	#(float) - How circular a contour has to be before it is counted.'
+		'\nthreshMethod = rel'
+		'\n	#(string) - (abs: absolute, rel: relative, dif: difference). See sections below.'
+		'\n\n#Process Thresholds determine the brightness used to create the first black-and-white image.'
+		'\n#Cell Thresholds determine the brightness below which the average pixel brightness within a contour must be to be counted as a cell.'
+		'\n\n[ABSOLUTE THRESHOLD] - (int) Uses a single threshold value across images. Higher values are less restrictive.'
+		'\nprocessThresh = 144'
+		'\ncellThresh = 134'
+		'\n\n[RELATIVE THRESHOLD] - (float) Determines threshold value for each image as a proportion of background brightness. Higher values are less restrictive.'
+		'\nprocessThresh = 0.83'
+		'\ncellThresh = 0.78'
+		'\n\n[DIFFERENCE THRESHOLD] - (int) Determines threshold for each image as a difference from background brightness. Higher values are more restrictive.'
+		'\nprocessThresh = 30'
+		'\ncellThresh = 40'
 	]
 	cfgFile.writelines(lines)
 	cfgFile.close()
@@ -60,16 +52,12 @@ config.read('config.ini')
 
 #Grab parameters from 'config.ini'
 mode = config.get('GENERAL','mode')
-fluorescent = config.getboolean('GENERAL','fluorescent')
-threshMethod = config.get('GENERAL','threshMethod')
 imageLocation = config.get('GENERAL','imageLocation')
 csvOutputLocation = config.get('GENERAL','csvOutputLocation')
 imageOutputLocation = config.get('GENERAL','imageOutputLocation')
-
-size = config.getint('CELL FEATURES','size')
-minAreaCoeff = config.getfloat('CELL FEATURES','minAreaCoeff')
-maxAreaCoeff = config.getfloat('CELL FEATURES','maxAreaCoeff')
-circularityThresh = config.getfloat('CELL FEATURES','circularityThresh')
+size = config.getint('GENERAL','size')
+circularityThresh = config.getfloat('GENERAL','circularityThresh')
+threshMethod = config.get('GENERAL','threshMethod')
 
 absProcessThresh = config.getint('ABSOLUTE THRESHOLD','processThresh')
 absCellThresh = config.getint('ABSOLUTE THRESHOLD','cellThresh')
@@ -107,26 +95,23 @@ processThresholds = []
 cellThresholds = []
 
 #Calculate minumum and maximum areas for filtering
-minArea = size**2 * minAreaCoeff
-maxArea = size**2 * maxAreaCoeff
+minArea = size**2 * 0.8
+maxArea = size**2 * 4.5
 
 for root, dirs, files in os.walk(imageLocation, topdown=False):
 	#Skip file if it has '_' in it, denoting some sort of output file
 	for f in files:
-		inputImg = os.path.join(root, f)
-		if imghdr.what(os.path.join(root, f)) == 'None':
+		if 'abs' in f or 'dif' in f or 'rel' in f or '.csv' in f:
 			continue
-		if 'abs' in f or 'dif' in f or 'rel' in f:
-			continue
-		print(inputImg, flush=True)
 		
-		#Generate file names with paths for input and output images
+		#Put together file names and paths for input and output images
 		imList.append(f)
-		xLen = f.rfind('.')
+		inputImg = os.path.join(root, f)
+		print(inputImg, flush=True)
 		if imageOutputLocation == '':
-			outputImg = root + '/' + f[:xLen] + '_' + threshMethod + f[xLen:]
+			outputImg = root + '/' + f[:-4] + '_' + threshMethod + f[-4:]
 		else:
-			outputImg = imageOutputLocation + '/' + f[:xLen] + '_' + threshMethod + f[xLen:]
+			outputImg = imageOutputLocation + '/' + f[:-4] + '_' + threshMethod + f[-4:]
 		
 		images = [0,0,0,0,0,0]			#The object used to store the image at each stage of transformation
 		contours = [[],[],[],[]]		#The object used to store the list of contours after each stage of filtering
@@ -134,12 +119,8 @@ for root, dirs, files in os.walk(imageLocation, topdown=False):
 		
 	#Load image and transform it
 		images[0] = cv2.imread(inputImg)
-		#if fluorescent:
-			#images[0] = cv2.bitwise_not(images[0])
 		images[1] = cv2.cvtColor(images[0], cv2.COLOR_BGR2GRAY)
-		if fluorescent:
-			images[1] = cv2.bitwise_not(images[1])
-		images[5] = images[1]			#Saved for creating the image masks to determine average pixel darkness within a contour
+		images[5] = cv2.cvtColor(images[0], cv2.COLOR_BGR2GRAY)			#Saved for creating the image masks to determine average pixel darkness within a contour
 		
 		#Assess background brightness and calculate thresholds
 		thr = cv2.threshold(images[1], 0, 255, cv2.THRESH_OTSU)[0]
@@ -202,16 +183,16 @@ for root, dirs, files in os.walk(imageLocation, topdown=False):
 	#Debug image generation
 		if mode == 'debug':
 		#Draw contours
-			images[4] = cv2.drawContours(images[0], contours[0], -1, (0,0,240), 2)
+			images[4] = cv2.drawContours(images[0], contours[0], -1, (0,0,255), 2)
 			images[4] = cv2.drawContours(images[0], contours[1], -1, (0,140,255), 2)
 			images[4] = cv2.drawContours(images[0], contours[2], -1, (0,255,255), 2)
-			images[4] = cv2.drawContours(images[0], contours[3], -1, (200,160,0), 2)
+			images[4] = cv2.drawContours(images[0], contours[3], -1, (0,220,0), 2)
 			
 		#Notate counts
-			cv2.putText(images[4], '+!Area:'+str(len(contours[0])), (8,144), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,240), 2)
+			cv2.putText(images[4], '+!Area:'+str(len(contours[0])), (8,144), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
 			cv2.putText(images[4], '+!Circular:'+str(len(contours[1])), (8,108), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,140,255), 2)
 			cv2.putText(images[4], '+!Dark:'+str(len(contours[2])), (8,72), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,255), 2)
-			cv2.putText(images[4], 'Cells:'+str(len(contours[3])), (8,36), cv2.FONT_HERSHEY_SIMPLEX, 1, (200,160,0), 2)
+			cv2.putText(images[4], 'Cells:'+str(len(contours[3])), (8,36), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,220,0), 2)
 			
 		#Write debug image
 			cv2.imwrite(outputImg, images[4])
@@ -220,7 +201,7 @@ for root, dirs, files in os.walk(imageLocation, topdown=False):
 		#Record average contour area
 			areaList = []
 			for i in range(len(contours[3])):
-				areaList.append(cv2.contourArea(contours[3][i]))
+				areaList.append(cv2.contourArea(contours[3][i]))#(len(contours[3][i]))
 			if areaList == []:
 				areas.append('N/A')
 			else:
